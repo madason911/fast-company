32ed51c4f8215b93ecf50780cbfc70f2f677a09f
madason911 <31772534+madason911@users.noreply.github.com>
2023-05-25 23:21:09 +0300

    WIP

--- src/app/store/createStore.js
+++ src/app/store/createStore.js
@@ -1,13 +1,9 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import commentsReducer from "./comments";
-import professionsReducer from "./professions";
-import qualitiesReducer from "./qualities";
 import usersReducer from "./users";
 import teamsReducer from "./teams";
 
 const rootReducer = combineReducers({
-    qualities: qualitiesReducer,
-    professions: professionsReducer,
     users: usersReducer,
     teams: teamsReducer,
     comments: commentsReducer
